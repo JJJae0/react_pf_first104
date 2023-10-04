d4e1f071eaa1a14a9e499d7a7f09c1902d256104
JJJae0 <jaeyeong345@naver.com>
2023-10-04 14:26:20 +0900

    on 클래스 적용

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,11 +1,11 @@
 import Layout from '../../common/layout/Layout';
-// import styles from './Department.module.scss';
-// import clsx from 'clsx';
+import styles from './gallery.module.scss';
+//import clsx from 'clsx';
 
 export default function Gallery() {
 	return (
-		<Layout title={'Gallery'}>
-			<p>갤러리 페이지 입니다.</p>
+		<Layout title={'Gallery'} styleName={styles.gallery}>
+			<p>갤러리 페이지입니다.</p>
 		</Layout>
 	);
 }
